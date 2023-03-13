d2707cce921e049f02d25e02ea801f701a54ed60
xuyaming0800 <xuyaming@bytedance.com>
2023-03-13 09:38:12 +0800

    Merge branch 'master' into hpc
    # Conflicts:
    #	builder/ecs/step_config_volcengine_ecs.go

--- version/version.go
+++ version/version.go
@@ -3,7 +3,7 @@ package version
 import "github.com/hashicorp/packer-plugin-sdk/version"
 
 var (
-	Version           = "0.0.1"
+	Version           = "1.0.0"
 	VersionPrerelease = "dev"
 	PluginVersion     = version.InitializePluginVersion(Version, VersionPrerelease)
 )
